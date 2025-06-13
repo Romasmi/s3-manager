e75beeff232a21bbc3ba3b43b5eb49fb78c1c2b4
romasmi <rsmarsu@gmail.com>
2025-06-13 08:07:55 +0700

    Add command to return bucket info.

---
+++ cmd/bucket_info.go
@@ -0,0 +1,68 @@
+package cmd
+
+import (
+	"context"
+	"github.com/spf13/cobra"
+	"s3manager/internal/s3client"
+	"s3manager/pkg/utils"
+	"time"
+)
+
+var bucketInfoCmd = &cobra.Command{
+	Use:   "bucket-info",
+	Short: "Get comprehensive bucket information",
+	Long: `Get detailed information about the S3 bucket
+The bucket name is taken from the configuration file unless overridden with --bucket flag.`,
+	Example: `  # Get info for configured bucket
+  s3manager bucket-info
+
+  # Get info for specific bucket
+  s3manager bucket-info --bucket my-other-bucket
+
+  # Verbose output
+  s3manager bucket-info --verbose`,
+	Run: func(cmd *cobra.Command, args []string) {
+		runBucketInfo(cmd)
+	},
+}
+
+func runBucketInfo(cmd *cobra.Command) {
+	client, err := s3client.New(cfg)
+	if err != nil {
+		utils.PrintError(err, "bucket-info")
+		handleError(err, cmd, "bucket-info")
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+	defer cancel()
+
+	if isVerbose(cmd) {
+		cmd.Printf("Getting bucket information for: %s\n", getBucketName(cmd))
+	}
+
+	info, err := client.GetBucketInfo(ctx)
+	if err != nil {
+		utils.PrintError(err, "bucket-info")
+		handleError(err, cmd, "bucket-info")
+		return
+	}
+
+	if bucketFlag := getBucketName(cmd); bucketFlag != cfg.BucketName {
+		info.BucketName = bucketFlag
+	}
+
+	if err := utils.PrintJSON(info); err != nil {
+		utils.PrintError(err, "bucket-info")
+		handleError(err, cmd, "bucket-info")
+		return
+	}
+
+	if isVerbose(cmd) {
+		cmd.Printf("Bucket info retrieved successfully\n")
+	}
+}
+
+func init() {
+	bucketInfoCmd.Flags().Int("timeout", 300, "Timeout in seconds for the operation")
+}
---
+++ pkg/utils/format.go
@@ -0,0 +1,47 @@
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"s3manager/internal/models"
+	"time"
+)
+
+func FormatBytes(bytes int64) string {
+	const unit = 1024
+	if bytes < unit {
+		return fmt.Sprintf("%d B", bytes)
+	}
+	div, exp := int64(unit), 0
+	for n := bytes / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %cB", float64(bytes)/float64(div), "KMGTPE"[exp])
+}
+
+func PrintJSON(data interface{}) error {
+	jsonOutput, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		return fmt.Errorf("failed to marshal JSON: %w", err)
+	}
+	fmt.Println(string(jsonOutput))
+	return nil
+}
+
+func PrintError(err error, command string) {
+	errorResp := models.ErrorResponse{
+		Error:     err.Error(),
+		Timestamp: time.Now().Format(time.RFC3339),
+		Command:   command,
+	}
+	err = PrintJSON(errorResp)
+	if err != nil {
+		fmt.Printf("Failed to print error in JSON format: %v\n\n", err)
+		return
+	}
+}
+
+func FormatTime(t time.Time) string {
+	return t.Format(time.RFC3339)
+}
